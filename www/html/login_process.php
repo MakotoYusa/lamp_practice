17a530983faeec04b66dcd1f170ccbde88ae3a4f
MakotoYusa <macoyusa@gmail.com>
2021-03-27 18:46:41 +0900

    CSRF対策を再修正しました。

--- www/html/login_process.php
+++ www/html/login_process.php
@@ -30,4 +30,5 @@ if (is_valid_csrf_token($token)) {
   redirect_to(HOME_URL);
 } else {
   set_error('不正な操作が行われました。');
+  redirect_to(LOGIN_URL);
 }
--- www/html/signup_process.php
+++ www/html/signup_process.php
@@ -33,4 +33,5 @@ if (is_valid_csrf_token($token)) {
   redirect_to(HOME_URL);
 } else {
   set_error('不正な操作が行われました。');
+  redirect_to(SIGNUP_URL);
 }
